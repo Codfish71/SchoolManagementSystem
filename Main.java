a6395722bb277a4453a5e3962bdb0c0d9c7b4374
ThePcCriminal71 <prathameshpendal71@gmail.com>
2021-08-05 18:36:55 +0530

    Initial import

---
+++ Main.java
@@ -0,0 +1,33 @@
+package schoolFundManagemenet;
+
+//import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		//Students info
+		 
+		
+		//addStudent
+		//removeStudent
+		//payFee
+		//showFee
+		
+		
+		
+		//Teachers Section 
+		//addTeaacher
+		//showSalary
+		
+		
+		
+		
+		//School functions
+		
+		//showExpenses
+		//showIncome
+	}
+
+}
---
+++ School.java
@@ -0,0 +1,12 @@
+package schoolFundManagemenet;
+
+import java.util.List;
+
+public class School {
+	List<Student> students;
+	List<Teacher> teachers;
+	
+	
+	
+	
+}
