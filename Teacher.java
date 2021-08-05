b14ac80048057b1f12f402eb5465b044d7b6360e
Pratham <47088865+Codfish71@users.noreply.github.com>
2021-08-05 18:50:13 +0530

    Add files via upload
    Initial upload

---
+++ Teacher.java
@@ -0,0 +1,41 @@
+package schoolFundManagemenet;
+
+/**
+ * @author prath
+ * This class is for students
+ * information that represent a student
+ *
+ */
+
+
+public class Teacher {
+	private int id;
+	private String name;
+	private int salary;
+	public Teacher(int id, String name, int salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+}
